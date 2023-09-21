d31ae1cd14828c28a8c36d87fc69dc516d355a1f
Pau Go Si <paugosi@hotmail.com>
2023-09-21 15:24:05 +0200

    Model creation

---
+++ CreateAndAccessDatabase/Appendix-B/Repositories/ConnectionStringHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateAndAccessDatabase.Appendix_B.Repositories
+{
+    public class ConnectionStringHelper
+    {
+        public static string GetConnectionString()
+        {
+            SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder();
+            connectionStringBuilder.DataSource = "N-DK-01-01-6135\\SQLEXPRESS";
+            connectionStringBuilder.InitialCatalog = "Chinook";
+            connectionStringBuilder.IntegratedSecurity = true;
+            return connectionStringBuilder.ConnectionString;
+
+            /*Scaffold-DbContext "Data Source = N-DK-01-01-6135\SQLEXPRESS;
+             Initial Catalog = Chinook; Integrated Security = True;
+            Trust Server Certificate = True;" Microsoft.EntityFrameworkCore.SqlServer -o Model
+
+            */
+        }
+    }
+}
